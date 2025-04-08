4ffa263a11f361a1220cfe76de707e0d1ab51014
Colby Switzer <colbyswitzer3@gmail.com>
2025-04-08 14:20:14 -0400

    Added saveVehicle method to RentalSystem allowing vehicles entered to be saved in a vehicles.txt file in CSV format for use in sequential application lifecycles

--- RentalSystem.java
+++ RentalSystem.java
@@ -1,4 +1,6 @@
 import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -9,10 +11,11 @@ public class RentalSystem {
     private List<Customer> customers = new ArrayList<>();
     private RentalHistory rentalHistory = new RentalHistory();
 
-    private RentalSystem(){}
+    private RentalSystem() {
+    }
 
-    public static RentalSystem getInstance(){
-        if(instance == null){
+    public static RentalSystem getInstance() {
+        if (instance == null) {
             instance = new RentalSystem();
         }
         return instance;
@@ -20,6 +23,7 @@ public class RentalSystem {
 
     public void addVehicle(Vehicle vehicle) {
         vehicles.add(vehicle);
+        saveVehicle(vehicle);
     }
 
     public void addCustomer(Customer customer) {
@@ -31,8 +35,7 @@ public class RentalSystem {
             vehicle.setStatus(Vehicle.VehicleStatus.RENTED);
             rentalHistory.addRecord(new RentalRecord(vehicle, customer, date, amount, "RENT"));
             System.out.println("Vehicle rented to " + customer.getCustomerName());
-        }
-        else {
+        } else {
             System.out.println("Vehicle is not available for renting.");
         }
     }
@@ -42,36 +45,37 @@ public class RentalSystem {
             vehicle.setStatus(Vehicle.VehicleStatus.AVAILABLE);
             rentalHistory.addRecord(new RentalRecord(vehicle, customer, date, extraFees, "RETURN"));
             System.out.println("Vehicle returned by " + customer.getCustomerName());
-        }
-        else {
+        } else {
             System.out.println("Vehicle is not rented.");
         }
-    }    
+    }
 
     public void displayVehicles(boolean onlyAvailable) {
-    	System.out.println("|     Type         |\tPlate\t|\tMake\t|\tModel\t|\tYear\t|");
-    	System.out.println("---------------------------------------------------------------------------------");
-    	 
+        System.out.println("|     Type         |\tPlate\t|\tMake\t|\tModel\t|\tYear\t|");
+        System.out.println("---------------------------------------------------------------------------------");
+
         for (Vehicle v : vehicles) {
             if (!onlyAvailable || v.getStatus() == Vehicle.VehicleStatus.AVAILABLE) {
-                System.out.println("|     " + (v instanceof Car ? "Car          " : "Motorcycle   ") + "|\t" + v.getLicensePlate() + "\t|\t" + v.getMake() + "\t|\t" + v.getModel() + "\t|\t" + v.getYear() + "\t|\t");
+                System.out.println(
+                        "|     " + (v instanceof Car ? "Car          " : "Motorcycle   ") + "|\t" + v.getLicensePlate()
+                                + "\t|\t" + v.getMake() + "\t|\t" + v.getModel() + "\t|\t" + v.getYear() + "\t|\t");
             }
         }
         System.out.println();
     }
-    
+
     public void displayAllCustomers() {
         for (Customer c : customers) {
             System.out.println("  " + c.toString());
         }
     }
-    
+
     public void displayRentalHistory() {
         for (RentalRecord record : rentalHistory.getRentalHistory()) {
             System.out.println(record.toString());
         }
     }
-    
+
     public Vehicle findVehicleByPlate(String plate) {
         for (Vehicle v : vehicles) {
             if (v.getLicensePlate().equalsIgnoreCase(plate)) {
@@ -80,11 +84,49 @@ public class RentalSystem {
         }
         return null;
     }
-    
+
     public Customer findCustomerById(String id) {
         for (Customer c : customers)
             if (c.getCustomerId() == Integer.parseInt(id))
                 return c;
         return null;
+    }
+
+    private void saveVehicle(Vehicle vehicle) {
+        try (FileWriter writer = new FileWriter("vehicles.txt", true)) {
+            StringBuilder sb = new StringBuilder();
+
+            if (vehicle instanceof Car) {
+                Car car = (Car) vehicle;
+                sb.append("Car,")
+                        .append(car.getLicensePlate()).append(",")
+                        .append(car.getMake()).append(",")
+                        .append(car.getModel()).append(",")
+                        .append(car.getYear()).append(",")
+                        .append(car.getNumSeats());
+
+            } else if (vehicle instanceof Motorcycle) {
+                Motorcycle moto = (Motorcycle) vehicle;
+                sb.append("Motorcycle,")
+                        .append(moto.getLicensePlate()).append(",")
+                        .append(moto.getMake()).append(",")
+                        .append(moto.getModel()).append(",")
+                        .append(moto.getYear()).append(",")
+                        .append(moto.hasSidecar());
+            } else if (vehicle instanceof Truck) {
+                Truck truck = (Truck) vehicle;
+                sb.append("Truck,")
+                        .append(truck.getLicensePlate()).append(",")
+                        .append(truck.getMake()).append(",")
+                        .append(truck.getModel()).append(",")
+                        .append(truck.getYear()).append(",")
+                        .append(truck.getCargoCapacity());
+            }
+
+            writer.write(sb.toString() + "\n");
+
+        } catch (IOException e) {
+            System.out.println("Error saving vehicle: " + e.getMessage());
+        }
     }
 }
